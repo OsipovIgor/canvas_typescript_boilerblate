84b43e6911e1b7298b53cc14cf19458da5c388f4
Igor Osipov <osipov.kaluga@gmail.com>
2021-06-10 17:06:52 +0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+canvas.width = 512;
+canvas.height = 512;
